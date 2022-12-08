dd887a62b6d68f6ed4e4d91cbbf19b8373c9f9f6
leah wanjiku <leagm22@gmail.com>
2022-12-08 08:30:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  return word ===word.split("").reverse().join("");
 }
 
 /* 
   Add your pseudocode here
+  declare a function with parameter word 
+  reverse the input
+  if the reversed string===to the given input
+  return true else return false not a palindrome
 */
 
 /*
   Add written explanation of your solution here
+  we declare a function
+  which loops through the input  and returns true if its a palindrome
+   meaning the requirements are met and false if they are not met.
 */
 
 // You can run `node index.js` to view these console logs
